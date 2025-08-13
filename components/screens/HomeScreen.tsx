c36fd968005cedd30063c1bd8d1e1be35bdc1189
Kotebus666 <kotebus666@gmail.com>
2025-08-13 22:09:05 +0300

    Fix settings buttons in horizontal mode

--- components/screens/HomeScreen.tsx
+++ components/screens/HomeScreen.tsx
@@ -122,6 +122,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingTop: 5,
         padding: 10,
+        zIndex: 1,
     },
     iconButton: {
         marginLeft: 15,
