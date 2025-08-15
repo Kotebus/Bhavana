fad46a89ba1356ebcb45e1f27772544f65ebc4c9
Kotebus666 <kotebus666@gmail.com>
2025-08-15 11:50:09 +0300

    Cleanup deps

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,16 +5,19 @@ import {I18nextProvider} from "react-i18next";
 import i18n from "i18next";
 import {AudioProvider} from "@/components/contexts/AudioContext";
 import {StatusBar} from "expo-status-bar";
+import {SafeAreaView} from "react-native";
 
 export default function RootLayout() {
   return (
-      <AudioProvider>
-          <SettingsProvider>
-              <I18nextProvider i18n={i18n}>
-                  <StatusBar hidden />
-                  <AppNavigator/>
-              </I18nextProvider>
-          </SettingsProvider>
-      </AudioProvider>
+      <SafeAreaView>
+          <AudioProvider>
+              <SettingsProvider>
+                  <I18nextProvider i18n={i18n}>
+                      <StatusBar hidden />
+                      <AppNavigator/>
+                  </I18nextProvider>
+              </SettingsProvider>
+          </AudioProvider>
+      </SafeAreaView>
   );
 }
