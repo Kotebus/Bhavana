f04174b502c0c7dd1f8bc9e9c5fd945d1bf8a43b
Kotebus666 <kotebus666@gmail.com>
2025-08-15 12:02:40 +0300

    Cleanup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,19 @@
-import 'react-native-reanimated';
 import {AppNavigator} from "@/components/AppNavigator";
 import {SettingsProvider} from "@/components/contexts/SettingsContext";
 import {I18nextProvider} from "react-i18next";
 import i18n from "i18next";
 import {AudioProvider} from "@/components/contexts/AudioContext";
 import {StatusBar} from "expo-status-bar";
-import {SafeAreaView} from "react-native";
 
 export default function RootLayout() {
-  return (
-      <SafeAreaView>
-          <AudioProvider>
-              <SettingsProvider>
-                  <I18nextProvider i18n={i18n}>
-                      <StatusBar hidden />
-                      <AppNavigator/>
-                  </I18nextProvider>
-              </SettingsProvider>
-          </AudioProvider>
-      </SafeAreaView>
-  );
+    return (
+        <AudioProvider>
+            <SettingsProvider>
+                <I18nextProvider i18n={i18n}>
+                    <StatusBar hidden/>
+                    <AppNavigator/>
+                </I18nextProvider>
+            </SettingsProvider>
+        </AudioProvider>
+    );
 }
