eecd0ef42a7fc97ea534dd5a81d4e491b9896b00
Kotebus666 <kotebus666@gmail.com>
2025-08-18 14:07:51 +0300

    Fix deepLinks

--- components/common/LinksList.tsx
+++ components/common/LinksList.tsx
@@ -13,19 +13,28 @@ const OpenURLButton = ({url, deepLink, children}: IOpenURLButtonProps) => {
         if (deepLink) {
             const supportedDeepLink = await Linking.canOpenURL(deepLink);
             if (supportedDeepLink) {
-                await Linking.openURL(deepLink);
-                return;
+                try {
+                    await Linking.openURL(deepLink);
+                    return;
+                } catch (e) {
+                    console.error('Error while oped deeplink ' + deepLink, e);
+                }
             }
         }
 
-        // Checking if the link is supported for links with custom URL scheme.
-        const supported = await Linking.canOpenURL(url);
-
-        if (supported) {
-            // Opening the link with some app, if the URL scheme is "http" the web link should be opened
-            // by some browser in the mobile
+        try {
             await Linking.openURL(url);
+        } catch (e) {
+            console.error('Error while oped url ' + url, e);
         }
+        // // Checking if the link is supported for links with custom URL scheme.
+        // const supported = await Linking.canOpenURL(url);
+        //
+        // if (supported) {
+        //     // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+        //     // by some browser in the mobile
+        //     await Linking.openURL(url);
+        // }
     }, [deepLink, url]);
 
     return (
